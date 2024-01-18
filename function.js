26ba6631d46f24f560fb5fd6fecbb1e8847504e0
Burakkturk <burak1845turk@icloud.com>
2024-01-18 08:37:27 +0300

    first commit

---
+++ function.js
@@ -0,0 +1,32 @@
+
+
+
+function yazdirMerhaba(){
+//   alert("Merhaba BT7")
+console.log("Merhaba BT7")
+}
+
+
+
+
+yazdirMerhaba()
+
+
+yazdirMerhaba()
+yazdirMerhaba()
+
+
+
+function yazdirParametre(degiskenName){
+    //   alert("Merhaba BT7")
+    console.log("Merhaba BT7", degiskenName)
+    }
+    
+    
+    
+    
+    yazdirMerhaba("BT8")
+    
+    
+    yazdirMerhaba("BT9")
+    yazdirMerhaba("BT10")

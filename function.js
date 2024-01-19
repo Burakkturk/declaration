2c0e7ab9b481414b5e13eab958bfb886ecfd468d
Burakkturk <burak1845turk@icloud.com>
2024-01-19 04:10:22 +0300

    Even

--- function.js
+++ function.js
@@ -1,32 +1,67 @@
 
 
 
-function yazdirMerhaba(){
-//   alert("Merhaba BT7")
-console.log("Merhaba BT7")
-}
+// function yazdirMerhaba(){
+// //   alert("Merhaba BT7")
+// console.log("Merhaba BT7")
+// }
 
 
 
 
-yazdirMerhaba()
+// yazdirMerhaba()
 
 
-yazdirMerhaba()
-yazdirMerhaba()
+// yazdirMerhaba()
+// yazdirMerhaba()
 
 
 
-function yazdirParametre(degiskenName){
+function yazdirParametre(message,degiskenName = "GS"){
     //   alert("Merhaba BT7")
-    console.log("Merhaba BT7", degiskenName)
+    console.log(message, degiskenName)
     }
     
     
     
     
-    yazdirMerhaba("BT8")
-    
-    
-    yazdirMerhaba("BT9")
-    yazdirMerhaba("BT10")
+    yazdirParametre("Hola","BT8")
+    yazdirParametre("Salut","BT9")
+    yazdirParametre("Hola","BT10")
+    yazdirParametre("Halo")
+    yazdirParametre("Ehlen")
+
+function calculate(name, yob){
+const age = new Date().getFullYear() - yob
+console.log(`${name} is ${age} years old`)
+return age
+}
+console.log(calculate("Ahmet", 1990) )
+const ismetAge = calculate("İsmet", 1890)
+
+function add(){
+
+const result1 = add(3, 5)
+const result2 = add(5, 9)
+const result3 = addShort(5, 9)
+
+console.log(result1, result2)
+console.log(result3)
+}
+
+
+
+function isEvenOrOdd(num) {
+if(num%2 === 0){
+    return "EVEN"
+}else {
+return "ODD"
+}}
+
+
+const num = prompt("Please enter a number:")
+
+
+
+
+console.log(`${num} is ${isEvenOrOdd(num)}`)
